228f7b2a6ea89a63909bcaf3183620cae1261853
Oliver <shafeo@hotmail.com>
2026-08-15 23:51:40 +0100

    Add privacy-filtered family tree report

---
+++ family-tree/scripts/calendarevents.js
@@ -0,0 +1,13 @@
+months=['','January','February','March','April','May','June','July','August','September','October','November','December'];
+weekdays=['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
+SpecialDay=1;
+oDic = {
+'view':'View',
+'changes':'Apply changes',
+'print':'Printer friendly',
+'export':'Export events displayed',
+'filter':'Filter events',
+'jump':'Jump to month'}
+AddEvent(20260214,'Barry Laxton \x26 Sandra Hayter \x3Cbdo dir=\\\x27ltr\\\x27\x3E45th\x3C/bdo\x3E anniversary','anniversary','','family-BarryLaxtonAndSandraHayter-F119.htm','','','','');
+AddEvent(20270214,'Barry Laxton \x26 Sandra Hayter \x3Cbdo dir=\\\x27ltr\\\x27\x3E46th\x3C/bdo\x3E anniversary','anniversary','','family-BarryLaxtonAndSandraHayter-F119.htm','','','','');
+
